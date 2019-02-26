2227cfc5b100ef2ba35c95d8fbe145e580e4a64d
yx <454543091@qq.com>
2019-02-26 16:57:39 +0800

    last commit file

--- django_test/poll/models.py
+++ django_test/poll/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -7,8 +9,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     put_date = models.DateTimeField('date published')  # 日期
 
+    def __str__(self):
+        return self.question_text
+
+    def was_pulished_recently(self):
+        return self.put_date >= timezone.now - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)  # 整数类型
+
+    def __str__(self):
+        return self.choice_text
--- django_test/poll/views.py
+++ django_test/poll/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    talk = ""
-    name = request.GET.get('name', '')
-    for i in range(3):
-        talk += "hello,"+name+"\n"
-    return HttpResponse(talk)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'poll/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
